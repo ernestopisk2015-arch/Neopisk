ccd9b474a202aa36ca5d7a3a13978f9d169fd71f
ernestopisk2015-arch <ernestopisk2015@gmail.com>
2026-03-05 21:47:57 -0500

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -1,36 +1,37 @@
-// Neo Pisk SW - admin
-var CACHE = 'neopisk-admin-v2';
+// Neo Pisk admin - Service Worker
+// Updated: 1772765121
+var CACHE_NAME = 'neopisk-admin-v3';
+var URLS = ['./index.html', './manifest.json', './sw.js'];
 
-self.addEventListener('install', function(e) {
-  console.log('[SW] Install');
+self.addEventListener('install', function(event) {
+  console.log('[SW:admin] Installing...');
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(['./index.html', './manifest.json']);
-    }).catch(function(err) {
-      console.log('[SW] Cache error', err);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS);
     })
   );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[SW] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE; })
-            .map(function(k) { return caches.delete(k); })
-      );
-    }).then(function() {
-      return self.clients.claim();
-    })
+self.addEventListener('activate', function(event) {
+  console.log('[SW:admin] Activating...');
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(function(keys) {
+        return Promise.all(
+          keys.filter(function(k) { return k !== CACHE_NAME; })
+              .map(function(k) { return caches.delete(k); })
+        );
+      })
+    ])
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request).catch(function() {
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).catch(function() {
         return caches.match('./index.html');
       });
     })
